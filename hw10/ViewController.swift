b5ed1c74a3a2af135f26518685cb211ae4d18c06
NikolaiKamenshchikov <gnomvelikan@gmail.com>
2022-05-28 18:05:42 +0300

    Delete all the comments

--- hw10/ViewController.swift
+++ hw10/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  hw10
-//
-//  Created by Nikolai Kamenshchikov on 28.05.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
